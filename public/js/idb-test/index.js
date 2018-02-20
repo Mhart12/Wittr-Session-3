17162ef8a19ed5a21777a0b50624a8c8760bd1ba
Monica Hart <mhart2012@gmail.com>
2018-02-20 15:18:37 -0600

    cursoring through database

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,18 +1,20 @@
 import idb from 'idb';
 
-var dbPromise = idb.open('test-db', 3, function(upgradeDb) {
+var dbPromise = idb.open('test-db', 4, function(upgradeDb) {
   switch(upgradeDb.oldVersion) {
     case 0:
       var keyValStore = upgradeDb.createObjectStore('keyval');
       keyValStore.put("world", "hello");
-//original objectStore
     case 1:
       upgradeDb.createObjectStore('people', { keyPath: 'name' });
-  //creating another objectStore, have to change version of database too. name is the key
     case 2:
       var peopleStore = upgradeDb.transaction.objectStore('people');
       peopleStore.createIndex('animal', 'favoriteAnimal');
+    case 3:
+      peopleStore = upgradeDb.transaction.objectStore('people');
+      peopleStore.createIndex('age', 'age');
   }
+  // TODO: create an index on 'people' named 'age', ordered by 'age'
 });
 
 // read "hello" in "keyval"
@@ -37,12 +39,13 @@ dbPromise.then(function(db) {
 dbPromise.then(function(db) {
   var tx = db.transaction('keyval', 'readwrite');
   var keyValStore = tx.objectStore('keyval');
-  keyValStore.put('horse', 'favoriteAnimal');
+  keyValStore.put('cat', 'favoriteAnimal');
   return tx.complete;
 }).then(function() {
-  console.log('Added your favorite animal');
+  console.log('Added favoriteAnimal:cat to keyval');
 });
 
+// add people to "people"
 dbPromise.then(function(db) {
   var tx = db.transaction('people', 'readwrite');
   var peopleStore = tx.objectStore('people');
@@ -52,31 +55,65 @@ dbPromise.then(function(db) {
     age: 25,
     favoriteAnimal: 'dog'
   });
+
   peopleStore.put({
-    name: 'Samantha Walsworth',
-    age: 25,
+    name: 'Susan Keller',
+    age: 34,
     favoriteAnimal: 'cat'
   });
+
   peopleStore.put({
-    name: 'Samson Wright',
-    age: 25,
-    favoriteAnimal: 'unicorn'
+    name: 'Lillie Wolfe',
+    age: 28,
+    favoriteAnimal: 'dog'
   });
-  //.put is calling name property, instead of keystore
+
+  peopleStore.put({
+    name: 'Marc Stone',
+    age: 39,
+    favoriteAnimal: 'cat'
+  });
 
   return tx.complete;
 }).then(function() {
   console.log('People added');
 });
 
+// list all cat people
 dbPromise.then(function(db) {
   var tx = db.transaction('people');
   var peopleStore = tx.objectStore('people');
   var animalIndex = peopleStore.index('animal');
-  //pulls up animals alphabetically when making another case above for 'animal'
 
-  return animalIndex.getAll('cat');//you can pass queries like 'cat'
+  return animalIndex.getAll('cat');
 }).then(function(people) {
-  console.log('People:', people);
+  console.log('Cat people:', people);
 });
-//reading people in the store, in alphabetical order since name is the key in this instance
+
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  //create a transaction
+  var peopleStore = tx.objectStore('people');
+  //get the objectStore
+  var ageIndex = peopleStore.index('age');
+  //get the index
+  //return animalIndex.getAll('cat');
+//}).then(function(people) {
+  //console.log('Cat people:', people);
+//});
+  return ageIndex.openCursor();
+//}).then(function(cursor) {
+  //if (!cursor) return;
+  //return cursor.advance(2);
+}).then(function logPerson(cursor){
+  if(!cursor) return;
+  console.log('Cursored at:', cursor.value.name);
+  //cursor.update(newValue)
+  //cursor.delete()
+  return cursor.continue().then(logPerson);
+}).then(function() {
+  console.log('Done cursoring');
+});
+  //this returns cursor object respresenting the first item in the index
+  //and goes on through the list. Goes through the whole object store.
+  //Then/or shoots undefined if there isn't one or aren't any left
